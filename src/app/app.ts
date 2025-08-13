53112c4a9a53bbf74baeddcad96203efdf14e766
Christoph <c.wagner.germany@gmail.com>
2025-08-13 19:43:53 +0200

    app3

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,19 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from './services/product-service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { BehaviorSubject, catchError, combineLatest, debounceTime, distinctUntilChanged, map, of, shareReplay, startWith, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  of,
+  shareReplay,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
 
 export interface Product {
   id: number;
@@ -16,12 +28,12 @@ export interface Product {
   selector: 'app-root',
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('observable-demo');
 
-    private service = inject(ProductService);
+  private service = inject(ProductService);
 
   // UI state (Signals nur für Komfort – Fokus bleibt auf Observables)
   loading = signal(false);
@@ -38,29 +50,30 @@ export class App {
     startWith(this.searchCtrl.value),
     debounceTime(300),
     distinctUntilChanged(),
-    tap(() => { this.loading.set(true); this.error.set(''); }),
-    switchMap(q =>
+    tap(() => {
+      this.loading.set(true);
+      this.error.set('');
+    }),
+    switchMap((q) =>
       this.service.searchProducts(q).pipe(
-        catchError(err => {
+        catchError((err) => {
           this.error.set('Fehler beim Laden. (Evtl. CORS/Netzwerk in Dev-Umgebung)');
           return of([] as Product[]);
-        }),
-      ),
+        })
+      )
     ),
     tap(() => this.loading.set(false)),
-    shareReplay({ bufferSize: 1, refCount: true }),
+    shareReplay({ bufferSize: 1, refCount: true })
   );
 
-    // 4) combineLatest: Ergebnisse + Filter zusammenführen
+  // 4) combineLatest: Ergebnisse + Filter zusammenführen
   results$ = combineLatest([this.rawResults$, this.inStockOnly$]).pipe(
-    map(([items, inStock]) => inStock ? items.filter(i => i.stock > 0) : items),
-    shareReplay({ bufferSize: 1, refCount: true }),
+    map(([items, inStock]) => (inStock ? items.filter((i) => i.stock > 0) : items)),
+    shareReplay({ bufferSize: 1, refCount: true })
   );
 
   // 5) ViewModel für Template (ein Stream, eine async-Subscription)
-  vm$ = this.results$.pipe(
-    map(items => ({ items })),
-  );
+  vm$ = this.results$.pipe(map((items) => ({ items })));
 
   toggleInStock(ev: Event) {
     const checked = (ev.target as HTMLInputElement).checked;
